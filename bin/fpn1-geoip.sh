9e67f50915145cfbf7cd6b337852c4fe67ba592e
Stephen Arnold <nerdboy@gentoo.org>
2020-02-29 13:29:46 -0800

    bin/fpn1-geoip.sh: add script to check geoip via https
    Signed-off-by: Stephen Arnold <nerdboy@gentoo.org>

---
+++ bin/fpn1-geoip.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+# check-geoip.sh
+#
+
+#set -x
+
+PATH=/usr/bin:/bin:/usr/sbin:/sbin
+export LC_ALL=C
+
+
+# Fetch data from Ubuntu's geoip server, requires route to internet
+xml_file="/tmp/geoip-location.xml"
+log_file="/tmp/wget.log"
+timeout 30 wget -o $log_file -O - -q https://geoip.ubuntu.com/lookup > $xml_file
+IP_ADDR=$(cat $xml_file | sed -n -e 's/.*<Ip>\(.*\)<\/Ip>.*/\1/p')
+LAT_FULL=$(cat $xml_file | sed -n -e 's/.*<Latitude>\(.*\)<\/Latitude>.*/\1/p')
+LON_FULL=$(cat $xml_file | sed -n -e 's/.*<Longitude>\(.*\)<\/Longitude>.*/\1/p' | cut -d"-" -f2)
+CITY=$(cat $xml_file | sed -n -e 's/.*<City>\(.*\)<\/City>.*/\1/p')
+STATE=$(cat $xml_file | sed -n -e 's/.*<RegionCode>\(.*\)<\/RegionCode>.*/\1/p')
+COUNTRY=$(cat $xml_file | sed -n -e 's/.*<CountryCode>\(.*\)<\/CountryCode>.*/\1/p')
+ZIPCODE=$(cat $xml_file | sed -n -e 's/.*<ZipPostalCode>\(.*\)<\/ZipPostalCode>.*/\1/p')
+
+LAT=$(printf "%.2f" "$LAT_FULL")
+LON=$(printf "%.2f" "$LON_FULL")
+LOCATION="$CITY, $STATE $ZIPCODE ($COUNTRY)"
+
+echo "Your public IP and geolocation: ${IP_ADDR}, ${LOCATION}"
