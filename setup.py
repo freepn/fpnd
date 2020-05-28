9fe67ac4fc26a066449aadf214814b80ed980ee5
Stephen Arnold <nerdboy@gentoo.org>
2020-05-28 11:07:44 -0700

    setup.py: bump version for packaging
    Signed-off-by: Stephen Arnold <nerdboy@gentoo.org>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup
 
 
-__version__ = '0.8.3'
+__version__ = '0.8.4'
 
 # make setuptools happy with PEP 440-compliant post version
 # (enable this for patch releases)
