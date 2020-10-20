68cd0edb89c20dc588f458cc8ffbf7e1042e0cf3
Stephen L Arnold <nerdboy@gentoo.org>
2020-10-19 23:06:42 -0700

    test_do_cleanup: fix pytest.raises context for different OS/python envs
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- test/test_mock_api_get.py
+++ test/test_mock_api_get.py
@@ -502,10 +502,10 @@ def test_do_cleanup():
     get_state(cache)
     nodeState = AttrDict.from_nested_dict(stest.fpnState)
     # print(nodeState)
-    with pytest.raises(PermissionError):
+    with pytest.raises( (PermissionError, FileNotFoundError, KeyError) ):
         do_cleanup('./bin', '127.0.0.1')
 
-    with pytest.raises(PermissionError):
+    with pytest.raises( (PermissionError, FileNotFoundError, KeyError) ):
         do_cleanup(path='./bin')
 
 
